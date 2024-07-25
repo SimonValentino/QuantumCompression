2260fdfb3a54bf1fc35efed6e11d2df48ace7f67
Simon Valentino <simontvalentino@gmail.com>
2024-07-25 10:44:45 -0400

    Added a ipynb

---
+++ quantum_compression.py
@@ -0,0 +1,11 @@
+import qiskit
+from qiskit import QuantumRegister
+
+
+# computes a + b into b
+def adder(a: QuantumRegister, b: QuantumRegister):
+    pass
+
+
+def muller(a: QuantumRegister, b: QuantumRegister, output: QuantumRegister):
+    pass
