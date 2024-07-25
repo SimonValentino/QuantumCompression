9538381e7cf46f30f6d32bd89e50e94d201079d9
Simon Valentino <simontvalentino@gmail.com>
2024-07-25 17:28:29 -0400

    Added adder code

--- quantum_compression.py
+++ quantum_compression.py
@@ -1,10 +1,26 @@
+from qiskit import QuantumCircuit, Aer, QuantumRegister, ClassicalRegister
 import qiskit
-from qiskit import QuantumRegister
+import numpy as np
+from qiskit.circuit.library import QFT
+import numpy as np
 
 
 # computes a + b into b
+# a and b have to have the same size n
 def adder(a: QuantumRegister, b: QuantumRegister):
-    pass
+    n = len(a)
+    
+    qc = QuantumCircuit(a, b)
+    
+    qc.append(QFT(n).to_gate(), b)
+    
+    for j in range(n):
+        for k in range(j + 1):
+            qc.cp(2 * np.pi / (2 ** (j - k + 1)), a[k], b[j])
+    
+    qc.append(QFT(n).inverse().to_gate(), b)
+    
+    return qc
 
 
 def muller(a: QuantumRegister, b: QuantumRegister, output: QuantumRegister):
