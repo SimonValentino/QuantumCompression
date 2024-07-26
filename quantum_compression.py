8b9c62a39e166a1a84868dce393dd8fbc89286c8
Simon Valentino <simontvalentino@gmail.com>
2024-07-26 11:31:52 -0400

    added quantization matrix

--- quantum_compression.py
+++ quantum_compression.py
@@ -6,11 +6,88 @@ from qiskit.circuit.library.standard_gates import PhaseGate
 from PIL import Image
 from arithmetic_operations import adder, muller
 
+# Performs Quantum Fourier Transform
+# invert to do inverse and reverse to swap the ordering
+
+
+def qft(n, invert, reverse):
+    qc = QuantumCircuit(n)
+    for i in reversed(range(n)):
+        qc.h(i)
+        for j in reversed(range(i)):
+            qc.cp(np.pi * (2.0 ** (j - i)), j, i)
+
+    if reverse:
+        for i in range(n // 2):
+            qc.swap(i, n - i - 1)
+
+    if invert:
+        qc = qc.inverse()
+
+    return qc
+
+
+# computes a + b into b
+# adder shown in this paper https://arxiv.org/pdf/quant-ph/0008033
+def adder(a: QuantumRegister, b: QuantumRegister):
+    n = len(a)
+
+    qc = QuantumCircuit(a, b)
+
+    qc.append(qft(n, reverse=False).to_gate(), b)
+
+    for j in range(n):
+        for k in range(j + 1):
+            qc.cp(2 * np.pi / (2 ** (j - k + 1)), a[k], b[j])
+
+    qc.append(qft(n, invert=False, reverse=False).to_gate(), b)
+
+    return qc
+
+
+# multiplies a and b into output, which is initialized to 0s
+# multiplier shown in this paper https://arxiv.org/pdf/1411.5949
+def muller(a: QuantumRegister, b: QuantumRegister, output: QuantumRegister):
+    n = len(a)
+
+    qc = QuantumCircuit(a, b, output)
+
+    firstqft = qft(2 * n, False, False).to_gate()
+    firstqft.name = 'QFT'
+
+    qc.append(firstqft, output)
+
+    for j in range(1, n + 1):
+        for i in range(1, n + 1):
+            for k in range(1, 2 * n + 1):
+                rot = (2 * np.pi) / (2 ** (i + j + k - 2 * n))
+                qc.append(PhaseGate(rot).control(2),
+                          [a[n - j], b[n - i], output[k - 1]],)
+
+    secondqft = qft(2 * n, True, False).to_gate()
+    secondqft.name = 'iQFT'
+
+    qc.append(secondqft, output)
+
+    return qc
+
+
+def quantize():
+    quantization_matrix = np.array([
+        [16, 11, 10, 16, 24, 40, 51, 61],
+        [12, 12, 14, 19, 26, 58, 60, 55],
+        [14, 13, 16, 24, 40, 57, 69, 56],
+        [14, 17, 22, 29, 51, 87, 80, 62],
+        [18, 22, 37, 56, 68, 109, 103, 77],
+        [24, 35, 55, 64, 81, 104, 113, 92],
+        [49, 64, 78, 87, 103, 121, 120, 101],
+        [72, 92, 95, 98, 112, 100, 103, 99]
+    ])
+    pass
+
 
 # main
 img_name = "beaver"
 img = Image.open(f"imgs/{img_name}.png").convert("L")
 img_arr = np.asarray(img)
 print(img_arr)
-
-
