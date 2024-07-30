989430994ec73f32142a7ec514276fe44cdee9d4
Simon Valentino <simontvalentino@gmail.com>
2024-07-30 10:35:43 -0400

    added inverse discrete cosine transform and inverse quantization

--- quantum_compression.py
+++ quantum_compression.py
@@ -73,7 +73,7 @@ def muller(a: QuantumRegister, b: QuantumRegister, output: QuantumRegister):
 
 def c(u):
     if u == 0:
-        return 1/np.sqrt(2)
+        return 1 / np.sqrt(2)
     else:
         return 1
 
@@ -89,34 +89,55 @@ def dct(img_arr):
                     sum = 0
                     for p in range(0, 8):
                         for q in range(0, 8):
-                            sum += img_arr[i+p, j+q] * \
-                                np.cos((2*p+1)*k*np.pi/16) * \
-                                np.cos((2*q+1)*l*np.pi/16)
-                    sum *= 0.25 * c(k) * c(l)
-                    dct[i+k, j+l] = sum
+                            sum += img_arr[i + p, j + q] * \
+                                np.cos((2 * p + 1) * k * np.pi / 16) * \
+                                np.cos((2 * q + 1) * l * np.pi / 16)
+                    sum *= 1 / 4 * c(k) * c(l)
+                    dct[i + k, j + l] = sum
 
 
-def quantize(dct_coefficients):
-    quantization_matrix = np.array([
-        [16, 11, 10, 16, 24, 40, 51, 61],
-        [12, 12, 14, 19, 26, 58, 60, 55],
-        [14, 13, 16, 24, 40, 57, 69, 56],
-        [14, 17, 22, 29, 51, 87, 80, 62],
-        [18, 22, 37, 56, 68, 109, 103, 77],
-        [24, 35, 55, 64, 81, 104, 113, 92],
-        [49, 64, 78, 87, 103, 121, 120, 101],
-        [72, 92, 95, 98, 112, 100, 103, 99]
-    ])
+def idct(dct_coefficients):
+    # loop through 8x8 blocks
+    m, n = dct_coefficients.shape
+    idct = np.zeros(dct_coefficients.shape)
+    for i in range(0, m, 8):
+        for j in range(0, n, 8):
+            for k in range(0, 8):
+                for l in range(0, 8):
+                    sum = 0
+                    for p in range(0, 8):
+                        for q in range(0, 8):
+                            sum += c(p) * c(q) * dct_coefficients[i + p, j + q] * \
+                                np.cos((2 * k + 1) * p * np.pi / 16) * \
+                                np.cos((2 * l + 1) * q * np.pi / 16)
+                    sum *= 1 / 4
+                    idct[i + k, j + l] = sum
+    return idct
 
+
+def quantize(dct_coefficients, quantization_matrix):
     m, n = dct_coefficients.shape
     quantized = np.zeros(dct_coefficients.shape)
     for i in range(0, m, 8):
         for j in range(0, n, 8):
             for k in range(0, 8):
                 for l in range(0, 8):
-                    quantized[i+k, j+l] = np.round(dct_coefficients[i+k, j+l] / quantization_matrix[k][l])
+                    quantized[i + k, j + l] = np.round(
+                        dct_coefficients[i + k, j + l] / quantization_matrix[k][l])
 
     return quantized
+
+
+def dequantize(quantized_arr, quantization_matrix):
+    m, n = quantized_arr.shape
+    dequantized = np.zeros(quantized_arr.shape)
+    for i in range(0, m, 8):
+        for j in range(0, n, 8):
+            for k in range(0, 8):
+                for l in range(0, 8):
+                    dequantized[i + k, j + l] = quantized_arr[i + k, j + l] * quantization_matrix[k][l]
+
+    return dequantized
 
 
 # main
@@ -124,3 +145,14 @@ img_name = "beaver"
 img = Image.open(f"imgs/{img_name}.png").convert("L")
 img_arr = np.asarray(img)
 print(img_arr)
+
+quantization_matrix = np.array([
+    [16, 11, 10, 16, 24, 40, 51, 61],
+    [12, 12, 14, 19, 26, 58, 60, 55],
+    [14, 13, 16, 24, 40, 57, 69, 56],
+    [14, 17, 22, 29, 51, 87, 80, 62],
+    [18, 22, 37, 56, 68, 109, 103, 77],
+    [24, 35, 55, 64, 81, 104, 113, 92],
+    [49, 64, 78, 87, 103, 121, 120, 101],
+    [72, 92, 95, 98, 112, 100, 103, 99]
+])
